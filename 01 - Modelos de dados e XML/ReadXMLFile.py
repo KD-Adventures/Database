d496559370e96eb37f805af3e72f33603e2f3c86
Davi Boberg <daviboberg@outlook.com>
2017-03-12 18:10:37 -0300

    Trabalho 1 - XML, pronto

---
+++ 01 - Modelos de dados e XML/ReadXMLFile.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+
+from xml.dom.minidom import parse
+import xml.dom.minidom
+
+
+
+# Open XML document using minidom parser
+DOMTree = xml.dom.minidom.parse("marvel_simplificado.xml")
+
+universe = DOMTree.documentElement
+if universe.hasAttribute("name"):
+   print "Root element : %s" % universe.getAttribute("name")
+
+# Get all the heroes in the universe
+heroes = universe.getElementsByTagName("hero")
+
+tag = ["name", "popularity", "alignment", "gender","height_m","weight_kg","hometown",
+        "intelligence","strength","speed","durability","energy_Projection","fighting_Skills"]
+
+herois = open("dadosMarvel/herois.csv", "w")
+heroisGood = open("dadosMarvel/herois_good.csv", "w")
+heroisBad = open("dadosMarvel/herois_bad.csv", "w")
+
+# Print detail of each hero.
+goodCounter = 1
+badCounter = 1
+totalCounter = 1
+peso = 0
+IMCHulk = 0
+
+for hero in heroes:
+
+
+    peso += int(hero.getElementsByTagName("weight_kg")[0].childNodes[0].data)
+    if hero.getElementsByTagName("name")[0].childNodes[0].data == "Hulk":
+        IMCHulk = float(hero.getElementsByTagName("weight_kg")[0].childNodes[0].data) + float(hero.getElementsByTagName("height_m")[0].childNodes[0].data)**2
+    herois.write(str(totalCounter))
+    for i in tag:
+        herois.write(", ")
+        herois.write(hero.getElementsByTagName(i)[0].childNodes[0].data)
+
+    herois.write("\n")
+    totalCounter = totalCounter + 1
+
+
+    alignment = hero.getElementsByTagName('alignment')[0]
+
+    if alignment.childNodes[0].data == "Good":
+        heroisGood.write(str(goodCounter))
+        for i in tag:
+            heroisGood.write(", ")
+            heroisGood.write(hero.getElementsByTagName(i)[0].childNodes[0].data)
+
+        heroisGood.write("\n")
+        goodCounter = goodCounter + 1
+
+    elif alignment.childNodes[0].data == "Bad":
+        heroisBad.write(str(badCounter))
+        for i in tag:
+            heroisBad.write(", ")
+            heroisBad.write(hero.getElementsByTagName(i)[0].childNodes[0].data)
+
+        heroisBad.write("\n")
+        badCounter = badCounter + 1
+
+print "Proporcao bons/maus: ",goodCounter/badCounter
+print "Media de Peso: ", peso/totalCounter
+print "IMC Hulk: ", IMCHulk
--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 
-# Estrutura básica do trabalho de CSB30 - Introdução A Bancos De Dados
+# Estrutura bï¿½sica do trabalho de CSB30 - Introduï¿½ï¿½o A Bancos De Dados
 
-Modifique este aquivo README.md com as informações adequadas sobre o seu grupo.
+Modifique este aquivo README.md com as informaï¿½ï¿½es adequadas sobre o seu grupo.
 
 ## Integrantes do grupo
 
-Liste o nome, RA e o usuário GitLab para cada integrante do grupo.
+Liste o nome, RA e o usuï¿½rio GitLab para cada integrante do grupo.
 
-- Nome do integrante 1, RA do integrante 1, usuário GitLab do integrante 1
-- Nome do integrante 2, RA do integrante 2, usuário GitLab do integrante 2
-- Nome do integrante 3, RA do integrante 3, usuário GitLab do integrante 3
+- Davi Boberg, 1236644 , davib
+- Dennys William da Silva, 1288490, dennys1994
+- Lucas Kaminski de Freitas, 1236865, lucaskfreitas
 
-## Descrição da aplicação a ser desenvolvida 
+## Descriï¿½ï¿½o da aplicaï¿½ï¿½o a ser desenvolvida
 
-Descreva aqui uma visão geral da aplicação que será desenvolvida pelo grupo durante o semestre. **Este texto deverá ser escrito quando requisitado pelo professor.** O conteúdo vai evoluir à medida em que o grupo avança com a implementação.
+Descreva aqui uma visï¿½o geral da aplicaï¿½ï¿½o que serï¿½ desenvolvida pelo grupo durante o semestre. **Este texto deverï¿½ ser escrito quando requisitado pelo professor.** O conteï¿½do vai evoluir ï¿½ medida em que o grupo avanï¿½a com a implementaï¿½ï¿½o.
