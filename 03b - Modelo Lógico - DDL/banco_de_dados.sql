de0d9d3e0a8797f280478ee0ad2df3b29d10b9e6
Kaminski <lucaskfreitas@hotmail.com>
2017-03-30 23:32:50 -0300

    atualizacao arquivo banco_de_dados.sql

--- 03b - Modelo Lógico - DDL/banco_de_dados.sql
+++ 03b - Modelo Lógico - DDL/banco_de_dados.sql
@@ -5,6 +5,11 @@ CREATE TABLE Usuario (
 	PRIMARY KEY(Login)
 );
 
+INSERT INTO Usuario VALUES ('joaquim', 'Joaquim da Silva', 'curitiba');
+INSERT INTO Usuario VALUES ('gertrude', 'Gertrude Pereira Machado', 'sao paulo');
+INSERT INTO Usuario VALUES ('constantino', 'Constantino Lima', 'moscou');
+
+
 CREATE TABLE Conhece (
 	Login1 VARCHAR(30) NOT NULL,
 	Login2 VARCHAR(30) NOT NULL,
@@ -18,6 +23,14 @@ CREATE TABLE Conhece (
 			ON DELETE SET NULL
 			ON UPDATE NO ACTION
 );
+
+INSERT INTO Conhece VALUES ('joaquim', 'gertrude');
+INSERT INTO Conhece VALUES ('joaquim', 'constantino');
+INSERT INTO Conhece VALUES ('gertrude', 'joaquim');
+INSERT INTO Conhece VALUES ('gertrude', 'constantino');
+INSERT INTO Conhece VALUES ('constantino', 'joaquim');
+INSERT INTO Conhece VALUES ('constantino', 'gertrude');
+
 
 CREATE TABLE Bloqueia (
 	Login1 VARCHAR(30) NOT NULL,
@@ -34,15 +47,28 @@ CREATE TABLE Bloqueia (
 			ON UPDATE NO ACTION			
 );
 
+INSERT INTO Bloqueia VALUES ('joaquim', 'gertrude', 'nao sei');
+INSERT INTO Bloqueia VALUES ('joaquim', 'constantino', 'cara chato');
+INSERT INTO Bloqueia VALUES ('gertrude', 'joaquim', 'fala demais');
+INSERT INTO Bloqueia VALUES ('gertrude', 'constantino', 'insuportavel');
+INSERT INTO Bloqueia VALUES ('constantino', 'joaquim', 'nao sou obrigado');
+INSERT INTO Bloqueia VALUES ('constantino', 'gertrude', 'quero paz');
+
+
 CREATE TABLE Elenco (
-	Id_Elenco INTEGER NOT NULL,
+	Id_Elenco VARCHAR(30) NOT NULL,
 	Telefone INTEGER NOT NULL,
 	Endereço VARCHAR(50) NOT NULL,
 	PRIMARY KEY(Id_Elenco)
 );
 
+INSERT INTO Elenco VALUES ('john apple', 65132, 'rua margarida');
+INSERT INTO Elenco VALUES ('mary orange', 156316, 'avenida das dores');
+INSERT INTO Elenco VALUES ('morgan freeman', 8744, 'travessa antonio moura');
+
+
 CREATE TABLE Diretor (
-	Id_Elenco INTEGER NOT NULL,
+	Id_Elenco VARCHAR(30) NOT NULL,
 	PRIMARY KEY(Id_Elenco),
 	FOREIGN KEY(Id_Elenco)
 		REFERENCES Elenco(Id_Elenco)
@@ -50,20 +76,27 @@ CREATE TABLE Diretor (
 			ON UPDATE NO ACTION
 );
 
+INSERT INTO Diretor VALUES ('john apple');
+
+
 CREATE TABLE Ator (
-	Id_Elenco INTEGER NOT NULL,
+	Id_Elenco VARCHAR(30) NOT NULL,
 	PRIMARY KEY(Id_Elenco),
 	FOREIGN KEY(Id_Elenco)
 		REFERENCES Elenco(Id_Elenco)
 			ON DELETE CASCADE
 			ON UPDATE NO ACTION
 );
+
+INSERT INTO Ator VALUES ('mary orange');
+INSERT INTO Ator VALUES ('morgan freeman');
+
 
 CREATE TABLE Filme(
 	Id_Filme VARCHAR(4) NOT NULL,
 	Nome VARCHAR(30) NOT NULL,
-	Lançamento VARCHAR(10) NOT NULL,
-	Id_Diretor INTEGER NOT NULL,
+	Lancamento VARCHAR(10) NOT NULL,
+	Id_Diretor VARCHAR(30) NOT NULL,
 	PRIMARY KEY(Id_Filme),
 	FOREIGN KEY(Id_Diretor)
 		REFERENCES Diretor(Id_Elenco)
@@ -71,9 +104,14 @@ CREATE TABLE Filme(
 			ON UPDATE NO ACTION
 );
 
+INSERT INTO Filme VALUES ('abcd', 'filme ruim', '23/10/2003', 'john apple');
+INSERT INTO Filme VALUES ('dfas', 'filme pessimo', '10/12/2011', 'john apple');
+INSERT INTO Filme VALUES ('axdf', 'filme razoavel', '14/08/1998', 'john apple');
+
+
 CREATE TABLE Atua (
-	Id_Elenco INTEGER NOT NULL,
-	Id_Filme INTEGER NOT NULL,
+	Id_Elenco VARCHAR(30) NOT NULL,
+	Id_Filme VARCHAR(4) NOT NULL,
 	PRIMARY KEY(Id_Elenco,Id_Filme),
 	FOREIGN KEY(Id_Elenco)
 		REFERENCES Ator(Id_Elenco)
@@ -85,9 +123,17 @@ CREATE TABLE Atua (
 			ON UPDATE NO ACTION
 );
 
+INSERT INTO Atua VALUES ('mary orange', 'abcd');
+INSERT INTO Atua VALUES ('mary orange', 'dfas');
+INSERT INTO Atua VALUES ('mary orange', 'axdf');
+INSERT INTO Atua VALUES ('morgan freeman', 'abcd');
+INSERT INTO Atua VALUES ('morgan freeman', 'dfas');
+INSERT INTO Atua VALUES ('morgan freeman', 'axdf');
+
+
 CREATE TABLE GostaFilme (
 	Login VARCHAR(30) NOT NULL,
-	Id_Filme INTEGER NOT NULL,
+	Id_Filme VARCHAR(4) NOT NULL,
 	PRIMARY KEY(Login, Id_Filme),
 	FOREIGN KEY(Login)
 		REFERENCES Usuario(Login)
@@ -98,6 +144,14 @@ CREATE TABLE GostaFilme (
 			ON DELETE SET NULL
 			ON UPDATE NO ACTION
 );
+
+INSERT INTO GostaFilme VALUES ('joaquim', 'axdf');
+INSERT INTO GostaFilme VALUES ('joaquim', 'abcd');
+INSERT INTO GostaFilme VALUES ('gertrude', 'dfas');
+INSERT INTO GostaFilme VALUES ('gertrude', 'abcd');
+INSERT INTO GostaFilme VALUES ('constantino', 'dfas');
+INSERT INTO GostaFilme VALUES ('constantino', 'axdf');
+
 
 CREATE TABLE Categoria(
 	Nome_Categoria VARCHAR(30) NOT NULL,
