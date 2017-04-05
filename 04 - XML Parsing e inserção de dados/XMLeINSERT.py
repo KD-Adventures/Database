15fb30caed0ad622ea9f6c7ae27c6c566027d9de
Kaminski <lucaskfreitas@hotmail.com>
2017-04-05 17:38:04 -0300

    XMLeINSERT.py atualizado

--- 04 - XML Parsing e inserção de dados/XMLeINSERT.py
+++ 04 - XML Parsing e inserção de dados/XMLeINSERT.py
@@ -10,13 +10,37 @@ locais = ["http://dainf.ct.utfpr.edu.br/~gomesjr/BD1/data/person.xml", "http://d
           "http://dainf.ct.utfpr.edu.br/~gomesjr/BD1/data/movie.xml", "http://dainf.ct.utfpr.edu.br/~gomesjr/BD1/data/knows.xml"]
 
 
-tagsPessoa       = ['Usuario', 'Person', 'uri', 'name', 'homewtown']
+tagsPessoa       = ['Usuario', 'Person', 'uri', 'name', 'hometown']
 tagsGostaArtista = ['GostaArtista', 'LikesMusic', 'person', 'bandUri', 'rating']
 tagsGostaFilme   = ['GostaFilme','LikesMovie', 'person', 'movieUri', 'rating']
 tagsConhecidos   = ['Conhece', 'Knows', 'person', 'colleague']
 
 
 tags = [tagsPessoa, tagsGostaArtista, tagsGostaFilme, tagsConhecidos]
+
+
+def extraiTitulo(url):
+    pagina = urllib2.urlopen(url)
+
+    for linha in pagina:
+        if linha.find("</title>") != -1:
+            index_comeco = linha.find("<title>")
+            index_fim = linha.find("</title>")
+            titulo = linha[index_comeco + len("<title>"):index_fim]
+            break
+
+    return titulo
+
+
+def extraiDados(dados):
+    i = len(dados) - 1
+
+    while i > 0 and dados[i] != '/':
+        i -= 1
+
+    dados = dados[i+1:]
+
+    return dados
 
 
 def inserir(dados, entidade, arquivo):
@@ -34,6 +58,7 @@ def inserir(dados, entidade, arquivo):
 	arquivo.write(");\n")
 
 
+
 def getData(root, index):
     
     tag = tags[index]
@@ -44,11 +69,22 @@ def getData(root, index):
     for child in root.findall(tag[1]):
         elementos = []
         for atributo in tag[2:]:
-            if(child.get(atributo) == "" or child.get(atributo) == None):
+            if atributo == "movieUri":
+                dado_final = extraiTitulo(child.get(atributo))
+            elif child.get(atributo).startswith("http"):
+                dado_final = extraiDados(child.get(atributo))
+            else:
+                dado_final = child.get(atributo)       
+            
+            print dado_final
+
+            if(dado_final == "" or dado_final == None):
                 elementos.append("NULL")
             else:
-                elementos.append(child.get(atributo))        
+                elementos.append(dado_final)
         
+
+
         inserir(elementos, tag[0], file)
 
     file.close()
