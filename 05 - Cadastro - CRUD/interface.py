172aedeb4115f34d66575d9daeb8d48261c72016
davib <daviboberg@outlook.com>
2017-04-11 21:36:02 -0300

    05 - interface atualizado

--- 05 - Cadastro - CRUD/interface.py
+++ 05 - Cadastro - CRUD/interface.py
@@ -6,16 +6,23 @@ banco = sqlite3.connect("rede.db")
 cursor = banco.cursor()	
 
 
+def toArray(dado):
+
+	dados = []
+	for i in dado:
+		# para todo j ate o numero de elementos de i
+		for j in range(len(i)):
+			dados.append(i[j])
+	return dados
+
 # formata o texto
 def imprimir(dado):
 	
-	dados = []
-	for i in dado:
-		string = ""
-		# para todo j ate o numero de elementos de i
-		for j in range(len(i)):
-			string += i[j]
-		print string
+	toPrint = toArray(dado)
+	
+	for i in toPrint:			
+		print i
+
 
 def getFromDataBase(codigo):
 	
@@ -30,59 +37,139 @@ def getFromDataBase(codigo):
 def putOnDataBase(codigo):
 
 	cursor.execute(codigo)
+	# submete o codigo ao banco de dados
+	banco.commit()
 
-
-def checkIfExist(dado, local, condicao):
+def checkIfExist(dado, entidade, condicao):
 
 	# verifica se o retorno esta vazio
-	if getFromDataBase("SELECT " + dado + " FROM " + local + " WHERE " + condicao + ";") == []:
+	if getFromDataBase("SELECT " + dado + " FROM " + entidade + " WHERE " + condicao + ";") == []:
 		return False
 	else:
 		return True
 
-def apagar(table, row, dado):
+def apagar(entidade, row, dado):
 
-	putOnDataBase("DELETE FROM " + table + " WHERE " + row + " = " + "\'" + dado + "\'"+ ";")
-	# submete o codigo ao banco de dados
-	banco.commit()
+	putOnDataBase("DELETE FROM " + entidade + " WHERE " + row + " = " + "\'" + dado + "\'"+ ";")
+	
 
-def listar(dado, local, condicao):
+def inserir(entidade):
+	
+
+	cursor.row_factory = sqlite3.Row
+
+	table = cursor.execute("SELECT * FROM " +  entidade + ";")
+
+	string = "INSERT OR IGNORE INTO " + entidade + " VALUES ("		
+
+	# pega o nome das colunas
+	nomeColunas = table.fetchone().keys()
+	
+	dados = []
+	for i in nomeColunas:
+
+		input = raw_input("\nDigite " + i + " desejado: ")
+		
+		dados.append(input)
+		# 0 -1 significa ultima posicao
+		if i == nomeColunas[-1]:
+			string += "' " + input + "');"
+		else:
+			string += "' " + input + "', "
+
+	print "\nNovo usuario  \n"
+	
+	novoUsuario = ""
+	for i in dados:
+		# 0 -1 significa ultima posicao
+		if i == dados[-1]:
+			novoUsuario += i + "; "
+		else:
+			novoUsuario += i + ", "
+
+	print novoUsuario
+
+	confirmacao = raw_input("\nDigite S para confirmar ou N para cancelar: \n>> ")
+
+	if confirmacao == "S" or confirmacao == "s":
+		putOnDataBase(string)
+	
+
+def editar(usuario):
+
+
+	cursor.row_factory = sqlite3.Row
+	table = cursor.execute("SELECT * FROM " +  "Usuario" + ";")
+	# pega o nome das colunas
+	nomeColunas = table.fetchone().keys()
+	
+	elementos = toArray(getElements("*", "Usuario", " Usuario.Login = '" + usuario + "'"))
+
+	print "\n"
+	for i in range(len(elementos)):
+		print nomeColunas[i] + ": " + elementos[i]
+	
+
+	campoExiste = False
+	while not campoExiste: 
+		campoModificado = raw_input("\nDigite o nome do campo a ser atualizado ou 1 - para cancelar:\n>> ")
+
+		if input == "1":
+			return
+
+		campoExiste = False
+		for i in nomeColunas:
+			if campoModificado == i:
+				novoValor = raw_input("\nDigite " + i + ":\n>> ")
+				campoExiste = True
+				novo = input
+				dadoAtual = elementos[nomeColunas.index(i)]
+				string = "UPDATE Usuario SET Usuario." + campoModificado + "= '" + novoValor + "' WHERE Usuario." + campoModificado + "= '" + dadoAtual + "';"
+				print string
+				#putOnDataBase(string)
+
+
+def getElements(dado, entidade, condicao):
 	
 	if condicao == "NULL":
-		imprimir(getFromDataBase("SELECT " + dado + " FROM " + local + ";"))
+		return getFromDataBase("SELECT " + dado + " FROM " + entidade + ";")
 	else:
-		imprimir(getFromDataBase("SELECT " + dado + " FROM " + local + " Where " + condicao + ";"))
+		return getFromDataBase("SELECT " + dado + " FROM " + entidade + " Where " + condicao + ";")
+
+
+def listar(dado, entidade, condicao):
+
+	imprimir(getElements(dado, entidade, condicao))
 
 	input = raw_input("\nDigite: \n1 - Para Apagar um usuario \n2 - Para Editar um usuario \n3 - Para voltar para a pagina inicial \n>> ")
 
 	digitoValido = False
 	while not digitoValido:
 		
-		if input == "1":
+		if input == "1" or input == "2":
 			existente = False
+			digitoValido = True
 			while not existente:
 				
-				usuario = raw_input("Digite: \nO login do usuario ou\n1 - para retornar a pagina inicial:\n>> ")
+				usuario = raw_input("\nDigite: \nO login do usuario ou\n1 - para retornar a pagina inicial:\n>> ")
 				if usuario == "1":
 					digitoValido = True
 					break
+				
 				existente = checkIfExist("Usuario.Login", "Usuario", "Usuario.Login = '" + usuario + "'")
 
 				if not existente:
 					print "\nUsuario nao existente\n"
 					continue
-				
-				confirmacao = raw_input("Tem certeza que quer excluir " + usuario + "? (Y/N)\n>> ")
-				if confirmacao == "Y" or confirmacao == "y":
-					apagar("Usuario", "Usuario.Login", usuario)
 
-				digitoValido = True
+				if input == "1":
+					confirmacao = raw_input("Tem certeza que quer excluir " + usuario + "? \n  Digite S para sim ou N para nao: \n>> ")
+					if confirmacao == "S" or confirmacao == "s":
+						apagar("Usuario", "Usuario.Login", usuario)
 
-		elif input == "2":
-			usuario = raw_input("Digite o login do usuario:\n>>")
-			editar(usuario)
-			digitoValido = True
-		
+				else:
+					editar(usuario)
+
 		elif input == "3":
 			digitoValido = True
 		
@@ -94,9 +181,9 @@ def inicial():
 
 	sair = False
 	while not sair:
-		input = raw_input("Digite: \n1 - Para Inserir um novo usuario ou \n2 - Para listar os usuarios existentes\n>> ")
+		input = raw_input("\nDigite: \n1 - Para Inserir um novo usuario ou \n2 - Para listar os usuarios existentes\n>> ")
 		if input == "1":
-			inserir()
+			inserir("Usuario")
 		elif input == "2":
 			listar("Usuario.Login", "Usuario", "NULL")
 
