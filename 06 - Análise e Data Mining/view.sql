3a8a78ccbb88ecbadd8f32768906c7ec27149f95
davib <daviboberg@outlook.com>
2017-04-24 19:29:47 -0300

    Trabalho 06 - View

---
+++ 06 - Análise e Data Mining/view.sql
@@ -0,0 +1,29 @@
+CREATE VIEW ConheceNormalizada AS 
+	SELECT Conhece.Login1, Conhece.Login2 FROM Conhece 
+	UNION 
+	SELECT Conhece.Login2, Conhece.Login1 FROM Conhece ORDER BY Conhece.Login1 ASC;
+
+SELECT * FROM ConheceNormalizada;
+
+SELECT MesmoFilme.Login1, MesmoFilme.Login2, Count(*) FROM ( 
+	SELECT ConheceNormalizada.*, GostaFilme.URI_Filme FROM ConheceNormalizada, GostaFilme
+	WHERE ConheceNormalizada.Login1 = GostaFilme.Login
+	INTERSECT
+	SELECT ConheceNormalizada.*,GostaFilme.URI_Filme FROM ConheceNormalizada, GostaFilme
+	WHERE ConheceNormalizada.Login2 = GostaFilme.Login
+	) AS MesmoFilme GROUP BY MesmoFilme.Login1, MesmoFilme.Login2;
+
+
+SELECT ConheceNormalizada.Login1, Conhecidos.* 
+	FROM (
+		SELECT ConheceNormalizada.Login1, COUNT(*)
+			FROM ConheceNormalizada
+			GROUP BY ConheceNormalizada.Login1 
+			ORDER BY ConheceNormalizada.Login1 ) AS Conhecidos, ConheceNormalizada
+
+	WHERE ConheceNormalizada.Login1 = "davib" AND Conhecidos.Login1 = ConheceNormalizada.Login2
+		OR 
+		ConheceNormalizada.Login1 = "lucasfreitas" AND Conhecidos.Login1 = ConheceNormalizada.Login2
+		OR 
+		ConheceNormalizada.Login1 = "dennyssilva" AND Conhecidos.Login1 = ConheceNormalizada.Login2
+	ORDER BY ConheceNormalizada.Login1 ASC;
