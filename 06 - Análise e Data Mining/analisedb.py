ec2eca5d385fbf24385f754097d376bbd29ed793
davib <daviboberg@outlook.com>
2017-04-27 16:46:30 -0300

    06 atualizado

--- 06 - Análise e Data Mining/analisedb.py
+++ 06 - Análise e Data Mining/analisedb.py
@@ -12,54 +12,54 @@ def executar(comando):
 
 def imprime(dado):
 	for i in dado:
-		print i
+		print(i)
 
 
 
 
 #1
 def mediaFilmes():
-	print 'Media dos filmes: '
+	print('Media dos filmes: ')
 	executar	(	
 						'''
 						SELECT AVG(GostaFilme.Nota) FROM GostaFilme;
 						'''
 					)
-	print cursor.fetchone()[0]
+	print(cursor.fetchone()[0])
 
 def mediaArtistas():
-	print 'Media dos artistas: '
+	print('Media dos artistas: ')
 	cursor.execute	(	
 						'''
 						SELECT AVG(GostaArtista.Nota) FROM GostaArtista;
 						'''
 					)
-	print cursor.fetchone()[0]
+	print(cursor.fetchone()[0])
 
 def desvioPadraoFilmes():
-	print 'Desvio padrao dos filmes: '
+	print('Desvio padrao dos filmes: ')
 	cursor.execute	(
 						'''
 						SELECT (SUM((G.Nota - Media)*(G.Nota - Media)))/Quantidade FROM GostaFilme AS G,
 							(SELECT AVG(G.Nota) AS Media, COUNT(G.Nota) AS Quantidade FROM GostaFilme AS G);
 						'''
 					)
-	print math.sqrt(cursor.fetchone()[0])
+	print(math.sqrt(cursor.fetchone()[0]))
 
 def desvioPadraoArtistas():
-	print 'Desvio padrao dos artistas: '
+	print('Desvio padrao dos artistas: ')
 	executar	(
 						'''
 						SELECT (SUM((G.Nota - Media)*(G.Nota - Media)))/Quantidade FROM GostaArtista AS G,
 							(SELECT AVG(G.Nota) AS Media, COUNT(G.Nota) AS Quantidade FROM GostaArtista AS G);
 						'''
 					)
-	print math.sqrt(cursor.fetchone()[0])
+	print(math.sqrt(cursor.fetchone()[0]))
 
 
 #2
 def maiorRatingMedioFilmes():
-	print 'Filmes com maior rating medio: '
+	print('Filmes com maior rating medio: ')
 	executar	(	
 						'''
 						SELECT Filme.Nome_Filme, AVG(GostaFilme.Nota) AS Media
@@ -75,7 +75,7 @@ def maiorRatingMedioFilmes():
 
 
 def maiorRatingMedioArtistas():
-	print 'Artistas com maior rating medio: '
+	print('Artistas com maior rating medio: ')
 	executar	(	
 						'''
 						SELECT Artista.Nome_Artista, AVG(GostaArtista.Nota) AS Media
@@ -95,7 +95,7 @@ def maiorRatingMedioArtistas():
 #3
 
 def filmesPopulares():
-	print 'Dez filmes mais populares: '
+	print('Dez filmes mais populares: ')
 	executar	(
 						'''
 						SELECT Filme.Nome_Filme, COUNT(Filme.URI_Filme) AS Quantidade
@@ -112,7 +112,7 @@ def filmesPopulares():
 
 
 def artistasPopulares():
-	print 'Dez artistas mais populares: '
+	print('Dez artistas mais populares: ')
 	executar	(
 						'''
 						SELECT Artista.Nome_Artista, COUNT(Artista.URI_Artista) AS Quantidade
@@ -128,45 +128,74 @@ def artistasPopulares():
 		imprime(linha)
 
 def view():
-	executar("""CREATE VIEW IF NOT EXISTS ConheceNormalizada AS SELECT Conhece.Login1, Conhece.Login2 FROM Conhece 
-		UNION SELECT Conhece.Login2, Conhece.Login1 FROM Conhece ORDER BY Conhece.Login1 ASC""")
-	leitura = cursor.fetchall()
-	for linha in leitura:
-		imprime(linha)
-	executar("SELECT * FROM ConheceNormalizada;")
-	leitura = cursor.fetchall()
-	for linha in leitura:
-		imprime(linha)
+	print('View ConheceNomralizada: ')
+	cursor.execute	(
+						'''
+						CREATE VIEW IF NOT EXISTS ConheceNormalizada AS 
+							SELECT Conhece.Login1, Conhece.Login2 FROM Conhece 
+							UNION 
+							SELECT Conhece.Login2, Conhece.Login1 FROM Conhece 
+							ORDER BY Conhece.Login1 ASC;
+							'''
+					)
+
+	cursor.execute	(
+						'''
+						SELECT * FROM ConheceNormalizada;
+						'''
+					)
+	for i in cursor.fetchall():
+		print(i)
+
 
 def gostaMesmoFilme():
-	executar("""SELECT MesmoFilme.Login1, MesmoFilme.Login2, Count(*) FROM ( 
-		SELECT ConheceNormalizada.*, GostaFilme.URI_Filme FROM ConheceNormalizada, GostaFilme
-			WHERE ConheceNormalizada.Login1 = GostaFilme.Login
-		INTERSECT
-		SELECT ConheceNormalizada.*,GostaFilme.URI_Filme FROM ConheceNormalizada, GostaFilme
-			WHERE ConheceNormalizada.Login2 = GostaFilme.Login
-		) AS MesmoFilme GROUP BY MesmoFilme.Login1, MesmoFilme.Login2;""")
-	leitura = cursor.fetchall()
-	for linha in leitura:
-		imprime(linha)
+	print('Conhecidos com mais filmes em comum: ')
+	cursor.execute	(
+						"""
+						SELECT coisa.login1, coisa.login2 
+							FROM (
+								SELECT MesmoFilme.Login1 as login1, MesmoFilme.Login2 as login2, COUNT(*) as soma
+									FROM ( 
+										SELECT ConheceNormalizada.*, GostaFilme.URI_Filme 
+											FROM ConheceNormalizada INNER JOIN GostaFilme ON ConheceNormalizada.Login1 = GostaFilme.Login
+										INTERSECT
+										SELECT ConheceNormalizada.*,GostaFilme.URI_Filme 
+											FROM ConheceNormalizada INNER JOIN GostaFilme ON ConheceNormalizada.Login2 = GostaFilme.Login
+									) AS MesmoFilme 
+									GROUP BY MesmoFilme.Login1, MesmoFilme.Login2
+							) AS coisa
+							GROUP BY coisa.soma
+							Having coisa.soma = MAX(coisa.soma);
+						"""
+					)
+
+	for i in cursor.fetchall():
+		print(i)
+
 
 def conhecidosDosConhecidos():
-	executar("""SELECT ConheceNormalizada.Login1, Conhecidos.* 
-		FROM (
-		SELECT ConheceNormalizada.Login1, COUNT(*)
-			FROM ConheceNormalizada
-			GROUP BY ConheceNormalizada.Login1 
-			ORDER BY ConheceNormalizada.Login1 ) AS Conhecidos, ConheceNormalizada
+	print('Conhecidos dos integrantes do grupo: ')
+	cursor.execute	(
+						"""
+						SELECT ConheceNormalizada.Login1, Conhecidos.* 
+							FROM (
+								SELECT ConheceNormalizada.Login1, COUNT(*)
+									FROM ConheceNormalizada
+									GROUP BY ConheceNormalizada.Login1 
+									ORDER BY ConheceNormalizada.Login1 
+								) AS Conhecidos, ConheceNormalizada
 
-		WHERE ConheceNormalizada.Login1 = "davib" AND Conhecidos.Login1 = ConheceNormalizada.Login2
-			OR 
-			ConheceNormalizada.Login1 = "lucasfreitas" AND Conhecidos.Login1 = ConheceNormalizada.Login2
-			OR 
-			ConheceNormalizada.Login1 = "dennyssilva" AND Conhecidos.Login1 = ConheceNormalizada.Login2
-		ORDER BY ConheceNormalizada.Login1 ASC;""")
-	leitura = cursor.fetchall()
-	for linha in leitura:
-		imprime(linha)
+							WHERE ConheceNormalizada.Login1 = "davib" AND Conhecidos.Login1 = ConheceNormalizada.Login2
+								OR 
+								ConheceNormalizada.Login1 = "lucasfreitas" AND Conhecidos.Login1 = ConheceNormalizada.Login2
+								OR 
+								ConheceNormalizada.Login1 = "dennyssilva" AND Conhecidos.Login1 = ConheceNormalizada.Login2
+							ORDER BY ConheceNormalizada.Login1 ASC;
+						"""
+					)
+	for i in cursor.fetchall():
+		print(i)
+
 
 def artistasMaisCurtidos():
 	executar("""SELECT
@@ -222,56 +251,55 @@ def gerarGrafico(x,y,xlabel,ylabel,tam):#dados do eixo x, daodos do eixo y, nome
 	plt.xlabel(xlabel)
 	plt.subplots_adjust(bottom=0.6)
 	plt.show()#mostra o grafico
+
 def clear():
 	os.system('cls' if os.name == 'nt' else 'clear')
 
 mediaFilmes()
-raw_input('Aperte enter para continuar\n')
+input('Aperte enter para continuar\n')
 clear()
 mediaArtistas()
-raw_input('Aperte enter para continuar\n')
+input('Aperte enter para continuar\n')
 clear()
 desvioPadraoFilmes()
-raw_input('Aperte enter para continuar\n')
+input('Aperte enter para continuar\n')
 clear()
 desvioPadraoArtistas()
-raw_input('Aperte enter para continuar\n')
+input('Aperte enter para continuar\n')
 clear()
 maiorRatingMedioFilmes()
-raw_input('Aperte enter para continuar\n')
+input('Aperte enter para continuar\n')
 clear()
 maiorRatingMedioArtistas()
-raw_input('Aperte enter para continuar\n')
+input('Aperte enter para continuar\n')
 clear()
 filmesPopulares()
-raw_input('Aperte enter para continuar\n')
+input('Aperte enter para continuar\n')
 clear()
 artistasPopulares()
-raw_input('Aperte enter para continuar\n')
+
+input('Aperte enter para continuar\n')
 clear()
-print'ConheceNormalizada'
 view()
-raw_input('Aperte enter para continuar\n')
+input('Aperte enter para continuar\n')
 clear()
-print'Gosta do mesmo Filme'
 gostaMesmoFilme()
-raw_input('Aperte enter para continuar\n')
+input('Aperte enter para continuar\n')
 clear()
-print'Conhecido dos conhecidos'
 conhecidosDosConhecidos()
-clear()
-print'Artistam mais gostados'
+
+print('Artistam mais gostados')
 dadosGraficos(artistasMaisCurtidos,'Artista Musical','Pessoas', 10)
-raw_input('Aperte enter para continuar\n')
+input('Aperte enter para continuar\n')
 clear()
-print'Filmes mais gostados'
+print('Filmes mais gostados')
 dadosGraficos(filmesMaisCurtidos,'Filmes','Pessoas', 10)
-raw_input('Aperte enter para continuar\n')
+input('Aperte enter para continuar\n')
 clear()
-print'Pessoas que mais gostaram de filmes'
+print('Pessoas que mais gostaram de filmes')
 dadosGraficos(pessoasQueMaisGostaramFilmes,'Pessoas','Numero de Filmes', 10)
-raw_input('Aperte enter para continuar\n')
+input('Aperte enter para continuar\n')
 clear()
-print'Pessoas que mais gostaram de Artistas'
+print('Pessoas que mais gostaram de Artistas')
 dadosGraficos(pessoasQueMaisGostaramArtistas,'Pessoas','Numero de Artistas', 10)
 clear()
