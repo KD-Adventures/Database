2cc6187c9d8479f58b1c4c54ea0f72f7877ff4cf
Kaminski <lucaskfreitas@hotmail.com>
2017-06-07 07:21:38 -0300

    filmes na interface

--- 10 - Etapa Final - Aplicação/main.py
+++ 10 - Etapa Final - Aplicação/main.py
@@ -3,6 +3,7 @@ import sqlite3
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 #from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty, NumericProperty
 from kivy.uix.gridlayout import GridLayout
@@ -15,11 +16,11 @@ cursor = banco.cursor()
 class RedeSocial(GridLayout):
 	nome_usuario_atual = StringProperty()
 	login_usuario_atual = StringProperty()
-	qtdade_amigos = NumericProperty(3)
+	qtdade_amigos = NumericProperty()
 
 
-	# Apresenta todas as informacoes na tela
-	def mostra_informacoes(self, *args):
+	# Apresenta na tela todos os amigos do usuario atual
+	def mostra_amigos(self, *args):
 		grid_amigos = self.ids.grid_amigos
 		grid_amigos.clear_widgets()
 		cursor.execute (""" select
@@ -32,10 +33,34 @@ class RedeSocial(GridLayout):
 		self.qtdade_amigos = len(amigos)
 		for amigo in amigos:
 			botao_amigo = Button(	text = amigo[0],
-									size_hint_y = 0.5,
 									background_color = [.5, .2, .2, 1])
 			botao_amigo.bind(on_press = partial(self.define_usuario, amigo[0]))
 			grid_amigos.add_widget(botao_amigo)
+
+
+	# Apresenta na tela todos os filmes curtidos pelo usuario atual
+	def mostra_filmes(self, *args):
+		grid_filmes = self.ids.grid_filmes
+		grid_filmes.clear_widgets()
+		cursor.execute (""" select
+								F.Nome_Filme
+							from
+								GostaFilme as G
+								inner join Filme as F on
+									G.URI_Filme = F.URI_Filme
+							where
+								G.Login = '""" + self.login_usuario_atual + "'")
+		filmes = cursor.fetchall()
+		for filme in filmes:
+			label_filme = Label(text = filme[0])
+			grid_filmes.add_widget(label_filme)
+
+
+	# Apresenta todas as informacoes do usuario atual na tela
+	def mostra_informacoes(self, *args):
+		self.mostra_amigos()
+		self.mostra_filmes()
+		
 
 
 	# Tenta carregar informacoes de outro usuario.
@@ -44,12 +69,11 @@ class RedeSocial(GridLayout):
 	def define_usuario(self, login, *args):
 		cursor.execute("select Nome_Usuario from Usuario as U where U.Login = '" + login + "'")
 		try:
-			backup = self.nome_usuario_atual
 			self.nome_usuario_atual = str(cursor.fetchone()[0]).title()
 			self.login_usuario_atual = login
 		except:
-			self.nome_usuario_atual = backup
 			return
+		self.ids.input_pesquisa.text = ''
 		self.mostra_informacoes()
 
 
